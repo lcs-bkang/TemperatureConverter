9a983b7e76416091634f5269163a250bedd46bfb
Bradley Kang <bkang@lcs.on.ca>
2021-02-02 14:59:54 -0500

    Finished celsius to fahrenheit function

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Functions
 
+// Celsius to Fahrenheit function
+func CtoF(Celsius c: Double) -> Double {
+    
+    // Create a variable to hold the fahrenheit value
+    var fahrenheit = 0.0
+    
+    // Code to convert celsius to fahrenheit
+    fahrenheit = (c * 9/5) + 32
+    
+    // Return the value
+    return fahrenheit
+}
+
+// MARK: Input
+
+// MARK: Process
+
+// MARK: Output
