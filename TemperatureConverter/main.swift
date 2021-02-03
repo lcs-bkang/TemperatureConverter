f8ec6fa75633e95be6f0230e16f5fefe036a86b8
Bradley Kang <bkang@lcs.on.ca>
2021-02-03 12:09:31 -0500

    Changed switch statements to encompass kelvin

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -17,57 +17,72 @@ print("========================================================================"
 print("What temperature scale are you converting from?")
 print("C: Celsius")
 print("F: Fahrenheit")
+print("K: Kelvin")
 print("Q: Quit the program")
 // Get their input on which one they are converting from
-let fromTempScaleLow = String.collectInput(withPrompt: "What temperature scale are you converting from (C / F / Q): ", acceptableValues: ["C", "F", "Q", "c", "f", "q"])
+let fromTempScaleLow = String.collectInput(withPrompt: "What temperature scale are you converting from (C / F / K / Q): ", acceptableValues: ["C", "F", "Q", "K", "c", "f", "q", "k"])
 let fromTempScale = fromTempScaleLow.uppercased()
-
+// Quit the program if they chose to
 switch fromTempScale {
 case "Q":
     print("The program has been quit.")
     exit(0)
 default:
-    print("")
-// Variable for the full names of the temperature scales.
-    var scaleName = ""
-    // Variable for degrees (temperature scale) at the end of the program
-    var fromDegrees = ""
-    var toDegrees = ""
-    // If statement to get a string to input to the string below.
-    switch fromTempScale {
-    case "C":
-        scaleName = "Celsius"
-        fromDegrees = "°C"
-    case "F":
-        scaleName = "Fahrenheit"
-        fromDegrees = "°F"
-    case "K":
-        scaleName = "Kelvin"
-        fromDegrees = "°K"
-    default:
-        break
-    }
-    // Switch the toDegrees depending on the converted temperature scale
-    
-    
-    // Ask and get the number they are converting.
-    let numberToConvert = Double.collectInputDouble(withPrompt: "What number on the \(scaleName) temperature scale are you converting? \n", minimum: nil, maximum: nil)
-    
-    // MARK: Process
-    
-    // Create a variable for the output
-    var output = 0.0
-    // Change the function that is called depending on the temperature scale entered.
-    switch fromTempScale {
-    case "C":
-        output = CtoF(Celsius: numberToConvert)
-    case "F":
-        output = FtoC(Fahrenheit: numberToConvert)
-    default:
-        break
-    }
-    // MARK: Output
-    print("The temperature \(numberToConvert)\(fromDegrees) is converted into \(output)\(toDegrees).")
+    break
 }
+// Ask for and get input for the temperature scale they are converting to
+let toTempScaleLow = String.collectInput(withPrompt: "What temperature scale are you converting to? (C / F / K) ", acceptableValues: ["C", "c", "F", "f", "K", "k"])
+let toTempScale = toTempScaleLow.uppercased()
+
+// Variable for the full names of the temperature scales.
+var scaleName = ""
+// Variable for degrees (temperature scale) at the end of the program
+var fromDegrees = ""
+var toDegrees = ""
+// If statement to get a string to input to the string below.
+switch fromTempScale {
+case "C":
+    scaleName = "Celsius"
+    fromDegrees = "°C"
+case "F":
+    scaleName = "Fahrenheit"
+    fromDegrees = "°F"
+case "K":
+    scaleName = "Kelvin"
+    fromDegrees = "°K"
+default:
+    break
+}
+// Switch the toDegrees depending on the converted temperature scale
+switch toTempScale {
+case "C":
+    toDegrees = "°C"
+case "F":
+    toDegrees = "°F"
+case "K":
+    toDegrees = "°K"
+default:
+    break
+}
+
+// Ask and get the number they are converting.
+let numberToConvert = Double.collectInputDouble(withPrompt: "What number on the \(scaleName) temperature scale are you converting? \n", minimum: nil, maximum: nil)
+
+// MARK: Process
+
+// Create a variable for the output
+var output = 0.0
+// Change the function that is called depending on the temperature scale entered.
+switch fromTempScale {
+case "C":
+    output = CtoF(Celsius: numberToConvert)
+case "F":
+    output = FtoC(Fahrenheit: numberToConvert)
+default:
+    break
+}
+// MARK: Output
+print("The temperature \(numberToConvert)\(fromDegrees) is converted into \(output)\(toDegrees).")
+
 
 
