f67cbf9c3c4fbc43e651610da6c420f45581b7cf
Bradley Kang <bkang@lcs.on.ca>
2021-02-04 12:50:29 -0500

    Switch functions for measurement names and other UI finished

--- Functions.swift
+++ Functions.swift
@@ -59,4 +59,78 @@ func convertKelvinToCelsius(Kelvin k: Double) -> Double {
 
 // MARK: Time Converter
 
-// 
+// Switch Function for from measurements.
+func fromMeasurementNames(from f: Int) -> String {
+    // Variable for from measurement name
+    var fromName = ""
+    
+    // Changing from measurement name
+    switch fromMeasurement {
+    case 1:
+        fromName = "Century"
+    case 2:
+        fromName = "Decade"
+    case 3:
+        fromName = "Year"
+    case 4:
+        fromName = "Month"
+    case 5:
+        fromName = "Days"
+    case 6:
+        fromName = "Hours"
+    case 7:
+        fromName = "Minutes"
+    case 8:
+        fromName = "Seconds"
+    case 9:
+        fromName = "Milliseconds"
+    case 10:
+        fromName = "Microseconds"
+    case 11:
+        fromName = "Nanoseconds"
+    default:
+        break
+    }
+    return fromName
+}
+// Switch function for to measurements.
+func toMeasurementNames(to t: Int) -> String {
+    // Variable to hold the name
+    var toName = ""
+    switch toMeasurement {
+    case 1:
+        toName = "Century"
+    case 2:
+        toName = "Decade"
+    case 3:
+        toName = "Year"
+    case 4:
+        toName = "Month"
+    case 5:
+        toName = "Days"
+    case 6:
+        toName = "Hours"
+    case 7:
+        toName = "Minutes"
+    case 8:
+        toName = "Seconds"
+    case 9:
+        toName = "Milliseconds"
+    case 10:
+        toName = "Microseconds"
+    case 11:
+        toName = "Nanoseconds"
+    default:
+        break
+    }
+    return toName
+}
+// Years to hours function
+func yearsToHours(Years y: Double) -> Double {
+    // Variable for output
+    var output = 0.0
+    // Code to convert
+    output = y * 8765.81
+    // Return
+    return output
+}
--- Time Converter/main.swift
+++ Time Converter/main.swift
@@ -8,15 +8,40 @@
 import Foundation
 
 // MARK: Input
-
+// Menu
 print("This is a time converter.")
 print("=========================")
 print("What measurement of time do you want to convert from?")
-print("Y: Years")
-print("MO: Months")
-print("D: Days")
-print("H: Hours")
-print("M: Minutes")
-print("S: Seconds")
-print("Q: Quit the program")
-let fromMeasurement = String.collectInput(withPrompt: "What measurement do you want to convert from? (Y / MO / D / H / M / S / Q): ", acceptableValues: ["Y", "y", "MO", "mo", "d", "D", "M", "m", "H", "h", "s", "S", "Q", "q"])
+print("1: Century")
+print("2: Decade")
+print("3: Year")
+print("4: Months")
+print("5: Days")
+print("6: Hours")
+print("7: Minutes")
+print("8: Seconds")
+print("9: Milliseconds")
+print("10: Microseconds")
+print("11: Nanoseconds")
+print("12: Quit the program")
+let fromMeasurement = Int.collectInputInt(withPrompt: "What measurement do you want to convert from? (1 / 2 / 3 / 4 / 5 / 6 / 7 / 8 / 9 / 10 / 11 / 12): ", minimum: 1, maximum: 12)
+// Function to choose what the name of the measurement they chose.
+let fromMeasurementName = fromMeasurementNames(from: fromMeasurement)
+
+// Checking if they chose to quit the program
+switch fromMeasurement {
+case 12:
+    print("")
+    print("The program has been quit.")
+    exit(0)
+default:
+    print("You chose the following measurement of time: \(fromMeasurementName).")
+}
+// Collect input for the type of measurement they are converting to.
+let toMeasurement = Int.collectInputInt(withPrompt: "WWhat measurement do you want to convert to? (1 / 2 / 3 / 4 / 5 / 6 / 7 / 8 / 9 / 10 / 11 ): ", minimum: 1, maximum: 12)
+// Function to choose what the name of measurement they are converting to.
+let toMeasurementName = toMeasurementNames(to: toMeasurement)
+// Tell them what time measurement they are converting to.
+print("You are converting to the following measurement of time: \(toMeasurementName)")
+
+
